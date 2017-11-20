a58bfbd210d4d33b65816619bc30ea12fbb2b157
Daniel Cohen <dlcohen224@gmail.com>
2017-11-20 18:07:25 -0500

    Feature: Articles now browsable by topic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function getArticles(){
 	year = $('#year').val();
 	month = $('#month').val();
+	topicSelection = $('#topic').val();
 	console.log(`year: ${year}, month: ${month}`)
 	var url = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json`;
 	url += '?' + $.param({
@@ -17,25 +18,30 @@ function getArticles(){
 	  })
 	  .then(response => response.json()) // parse response as JSON
 	  .then(data => {
-	  	var ol = document.getElementById("output-list")
-	  	while (ol.hasChildNodes()) {
-    		ol.removeChild(ol.lastChild);
+	  	var ul = document.getElementById("output-list")
+	  	while (ul.hasChildNodes()) {
+    		ul.removeChild(ul.lastChild);
 		}
 	  	for(doc of data["response"]["docs"]){
-	  		console.log(JSON.stringify(doc));
+	  		//console.log(JSON.stringify(doc));
 	  		var li = document.createElement("li");
 	  		var link = document.createElement("a");
-	  		link.href = doc["web_url"];
-	  		link.value = JSON.stringify(doc["headline"]["main"]);
-	  		link.target = "_blank"
-	  		li.appendChild(document.createTextNode(JSON.stringify(doc["headline"]["main"])));
-	  		link.appendChild(li)
-	  		ol.appendChild(link)
-	  	} 
-	    //document.getElementById("output").innerHTML = (`Data: ${JSON.stringify(data["response"]["docs"])}`)
+	  		//the topic is the item in the web url after the 6th slash
+	  		var topic = JSON.stringify(doc["web_url"]).split("/")[6]
+	  		if (topic == topicSelection){
+	  			link.href = doc["web_url"];
+		  		link.value = topic+JSON.stringify(doc["headline"]["main"]);
+		  		link.target = "_blank"
+		  		//append headline to li
+		  		li.appendChild(document.createTextNode(JSON.stringify(doc["headline"]["main"]).replace('"','').replace('"','')));
+		  		//append li to link
+		  		link.appendChild(li)
+		  		ul.appendChild(link)
+	  		}
+	  	}
+	  	//sortList(ul);
 	  })
 	  .catch(function(error) {
 	    console.log(`Error: ${error.message}`);
 	  });
 }
-//getArticles();
